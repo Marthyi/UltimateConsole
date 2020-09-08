d65584a32a02cafad18526dd5128033815876975
Olivier MARTINET <om@activeassurances.fr>
2020-09-08 12:26:02 +0200

    init project

---
+++ src/UltimateConsole/UltimateConsole.Sample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace UltimateConsole.Sample
+{
+    public class Operation
+    {
+        public int Id { get; set; }
+        public int sleepDuration { get; set; }
+    }
+
+    class Program
+    {
+        const int ConsoleSize = 20;
+
+        static async Task Main(string[] args)
+        {
+            ActionBlock<Operation> action = new ActionBlock<Operation>(ActionOperationAsync, new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = 10 });
+
+            var rnd = new Random();
+
+            SafeConsole.WriteLine($"Start program", new ConsoleOptions() { Left = 0, Top = 20 });
+
+            for (int i = 0; i < 100; i++)
+            {
+                DisplayOperation(i, ConsoleColor.Red);
+                var operation = new Operation() { Id = i, sleepDuration = rnd.Next(5000) };
+                SafeConsole.WriteLine($"sleep duration : {operation.sleepDuration}", new ConsoleOptions() { Left = 0, Top = 19 });
+                action.Post(operation);
+            }
+
+            action.Complete();
+
+            await action.Completion.ConfigureAwait(false);
+
+            Console.ReadKey();
+        }
+
+        private static void DisplayOperation(int id, ConsoleColor color)
+        {
+            SafeConsole.Write(" ", new ConsoleOptions()
+            {
+                Background = color,
+                Left = id % ConsoleSize,
+                Top = id / ConsoleSize,
+                HasToReturnToPreviousPosition = true                
+            });
+        }
+
+        private static async Task ActionOperationAsync(Operation operation)
+        {
+            DisplayOperation(operation.Id, ConsoleColor.Yellow);
+            await Task.Delay(operation.sleepDuration).ConfigureAwait(false);
+            DisplayOperation(operation.Id, ConsoleColor.Green);
+        }
+    }
+}
---
+++ src/UltimateConsole/UltimateConsole/SafeConsole.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace UltimateConsole
+{
+    public class ConsoleOptions
+    {
+        public int? Left { get; set; }
+        public int? Top { get; set; }
+
+        public bool HasToReturnToPreviousPosition { get; set; }
+
+        public ConsoleColor? ForegroundColor { get; set; }
+        public ConsoleColor? Background { get; set; }
+    }
+
+    public static class SafeConsole
+    {
+        private static readonly BlockingCollection<Action> _operations;
+        private static readonly Task _writerTask;
+
+        static SafeConsole()
+        {
+            _operations = new BlockingCollection<Action>();
+
+            _writerTask = Task.Run(() =>
+            {
+                foreach (Action action in _operations.GetConsumingEnumerable())
+                {
+                    action();
+                }
+            });
+        }
+
+        public static void WriteLine(string line) => _operations.Add(() => Console.WriteLine(line));
+        public static void Write(string line) => _operations.Add(() => Console.Write(line));
+
+        public static void WriteLine(string line, ConsoleOptions options)
+        {
+            _operations.Add(() =>
+            {
+                InternalWrite(line + Environment.NewLine, options);
+            });
+        }
+
+        public static void Write(string line, ConsoleOptions options)
+        {
+            _operations.Add((Action)(() =>
+            {
+                InternalWrite(line, options);
+            }));
+        }
+
+        private static void InternalWrite(string line, ConsoleOptions options)
+        {
+            int top = Console.CursorTop;
+            int left = Console.CursorLeft;
+
+            Console.CursorLeft = options.Left ?? Console.CursorLeft;
+            Console.CursorTop = options.Top ?? Console.CursorTop;
+
+            Console.ForegroundColor = options?.ForegroundColor ?? Console.ForegroundColor;
+            Console.BackgroundColor = options?.Background ?? Console.BackgroundColor;
+
+            Console.Write(line);
+
+            if (options.Background != null || options.ForegroundColor != null)
+            {
+                Console.ResetColor();
+            }
+            if (options.HasToReturnToPreviousPosition)
+            {
+                Console.SetCursorPosition(left, top);
+            }
+        }
+    }
+
+}
